e7e4b42f9a40aa698a7b282013617e90e50e19e2
natalia2911 <nataliamartir@correo.ugr.es>
2018-10-13 11:47:59 +0200

    Corrigiendo errores x3

--- src/funciones.py
+++ src/funciones.py
@@ -11,34 +11,34 @@ class Noticias:
 			with open('noticias.json', 'r') as f:
 				self.data = json.load(f)
 
-	def getNoticiasFecha(self, fecha):
-		noticia = []
-		for i in self.data:
-			if i["fecha"] == fecha:
-				for j in i["noticia"]:
-					noticia.append(j)
-			if not noticia: noticia = False 
-
-		return noticia
-		
-
-	def getNoticiasUsuario(self, usuario):
-		noticia = []
-		try:
-			for i in self.data[usuario-1]["noticia"]:
-				noticia.append(i["noticia"])
-			except:
-				noticia = False
+		def getNoticiasFecha(self, fecha):
+			noticia = []
+			for i in self.data:
+				if i["fecha"] == fecha:
+					for j in i["noticia"]:
+						noticia.append(j)
+				if not noticia: noticia = False 
 
 			return noticia
+			
 
-	def setNoticia(self, usuario, noticia):
-		fecha = date.today()
+		def getNoticiasUsuario(self, usuario):
+			noticia = []
 			try:
-				self.data[usuario-1]["noticia"].append({'usuario': usuario,'fecha': fecha, 'noticia': noticia})
-				#with open('noticias.json', 'w') as f:
-					#json.dump(self.data, f)
-				return True
-			except:
-				return False
+				for i in self.data[usuario-1]["noticia"]:
+					noticia.append(i["noticia"])
+				except:
+					noticia = False
+
+				return noticia
+
+		def setNoticia(self, usuario, noticia):
+			fecha = date.today()
+				try:
+					self.data[usuario-1]["noticia"].append({'usuario': usuario,'fecha': fecha, 'noticia': noticia})
+					#with open('noticias.json', 'w') as f:
+						#json.dump(self.data, f)
+					return True
+				except:
+					return False
 
