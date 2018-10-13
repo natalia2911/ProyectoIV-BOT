3be42bb93cf521654d9544499c55272f1f5806cc
natalia2911 <nataliamartir@correo.ugr.es>
2018-10-13 12:09:33 +0200

    Corrigiendo errores x6

--- src/funciones.py
+++ src/funciones.py
@@ -8,42 +8,37 @@ import calendar
 class Noticias:
 
 		def __init__(self):
-		with open('noticias.json', 'r') as f:
-			self.ap = json.load(f)
+			with open('noticias.json', 'r') as f:
+				self.ap = json.load(f)
 
-	def getNoticiasFecha(self, fecha):
+		def getNoticiasFecha(self, fecha):
+			noticia = []
+			for i in self.ap:
+				if i["fecha"] == fecha:
+					for j in i["noticia"]:
+						noticia.append(j)
+				if not noticia: noticia = False 
+			return noticia
+			
 
-		noticia = []
-
-		for i in self.ap:
-			if i["fecha"] == fecha:
-				for j in i["noticia"]:
-					noticia.append(j)
-			if not noticia: noticia = False 
-
-		return noticia
-		
-
-	def getNoticiasUsuario(self, usuario):
-
-		noticia = []
-		try:
-			for i in self.ap[usuario-1]["noticia"]:
-				noticia.append(i["noticia"])
-			except:
-				noticia = False
-
+		def getNoticiasUsuario(self, usuario):
+			noticia = []
+			try:
+				for i in self.ap[usuario-1]["noticia"]:
+					noticia.append(i["noticia"])
+				except:
+					noticia = False
 			return noticia
 
-	def setNoticia(self, usuario, noticia):
-		fecha = date.today()
-		try:
-			self.ap[usuario-1]["noticia"].append({"noticia": noticia})
-			with open('noticias.json', 'w') as f:
-				json.dump(self.ap, f)
-			return True
-		except:
-			return False
+		def setNoticia(self, usuario, noticia):
+			fecha = date.today()
+			try:
+				self.ap[usuario-1]["noticia"].append({"noticia": noticia})
+				with open('noticias.json', 'w') as f:
+					json.dump(self.ap, f)
+				return True
+			except:
+				return False
 
 
 
