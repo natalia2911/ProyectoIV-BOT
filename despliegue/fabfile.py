989e99cbc71591b2f29ad7bfc6b1449882b53720
natalia2911 <nataliamartir@correo.ugr.es>
2019-01-13 13:52:57 +0100

    Corregido fabfile close #59

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -1,12 +1,12 @@
 from fabric.api import *
 from fabric.contrib.console import confirm
-import os
+
 
 
 # Definimos una variable de entorno con el host al que nos vamos a conectar
 # y el nombre de usuario
 env.user = "vagrant"
-env.host = ['noticieroapp.westeurope.cloudapp.azure.com']
+env.host = ['noticiero.westeurope.cloudapp.azure.com']
 
 
 def Desinstalar():
@@ -17,10 +17,17 @@ def Instalar():
 	#Instalamos el servicio clonando eliminando el codigo anterior e instalando los requerimientos
 	Desinstalar()
 	run("git clone https://github.com/natalia2911/ProyectoIV-BOT")
-	run('cd ProyectoIV-BOT/  && sudo pip3 install -r requirements.txt')
+	with cd("ProyectoIV-BOT"):
+		run('pip3 install --user -r requirements.txt')
+
+def Actualizar():
+	#Hacemos un pull de proyecto para descargar las actualizaciones, y volvemos a instalar los requerimientos
+	with cd("ProyectoIV-BOT"):
+		run('git pull')
+		run('pip3 install --user -r requirements.txt')
 
 
 def Iniciar():
 	#Iniciamos el servicio
 	with cd("ProyectoIV-BOT/src/"):
-		sudo("gunicorn noticiero-app:app --log-file - &")
+		sudo("sudo gunicorn noticiero-app:app -b 0.0.0.0:80 &")
